10025f062295ab8653f226f0c0cb662c329f4735
Vidar Magnusson <vidar.halmstad@hotmail.com>
2019-04-10 11:34:28 +0200

    Add variable with json data to App.

--- app/parkit/App.tsx
+++ app/parkit/App.tsx
@@ -1,21 +1,50 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import List from "./use-cases/main/list/List";
+import data from "./assets/temp.json";
+
+export interface IPosition {
+  longitude: Number;
+  latitude: Number;
+}
+
+export class ParkingSpot {
+  position?: IPosition;
+  name?: String;
+  distance?: Number;
+  city?: String;
+  owner?: String;
+}
+
+export interface IParkingSpot {
+  position: IPosition;
+  name: String;
+  distance: Number;
+  city: String;
+  owner: String;
+}
 
 export default class App extends React.Component {
-    public render() {
-        return (
-            <View style={styles.container}>
-                <Text>Open up App.tsx to start working on your app!</Text>
-            </View>
-        );
-    }
+  constructor(props: any) {
+    super(props);
+
+    let spots = data.parkingspots as Array<IParkingSpot>;
+  }
+
+  public render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        backgroundColor: "#fff",
-        flex: 1,
-        justifyContent: "center",
-    },
+  container: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
+    justifyContent: "center"
+  }
 });
