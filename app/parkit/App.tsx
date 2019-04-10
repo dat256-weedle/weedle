4ff1da7a90ceefe9bb0d549d711a92d54f54b40b
Vidar Magnusson <vidar.halmstad@hotmail.com>
2019-04-10 11:45:49 +0200

    formatted files.

--- app/parkit/App.tsx
+++ app/parkit/App.tsx
@@ -4,26 +4,26 @@ import data from "./assets/temp.json";
 import { IParkingSpot } from "./types/ParkingSpots.js";
 
 export default class App extends React.Component {
-  constructor(props: any) {
-    super(props);
+    constructor(props: any) {
+        super(props);
 
-    let spots = data.parkingspots as Array<IParkingSpot>;
-  }
+        let spots = data.parkingspots as Array<IParkingSpot>;
+    }
 
-  public render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-      </View>
-    );
-  }
+    public render() {
+        return (
+            <View style={styles.container}>
+                <Text>Open up App.tsx to start working on your app!</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    backgroundColor: "#fff",
-    flex: 1,
-    justifyContent: "center"
-  }
+    container: {
+        alignItems: "center",
+        backgroundColor: "#fff",
+        flex: 1,
+        justifyContent: "center"
+    }
 });
