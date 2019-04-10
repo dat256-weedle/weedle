819f7993d7986a87da3206f0d5f0034a16e73677
Vidar Magnusson <vidar.halmstad@hotmail.com>
2019-04-10 11:45:49 +0200

    formatted files.

--- app/parkit/types/ParkingSpots.tsx
+++ app/parkit/types/ParkingSpots.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 export interface IPosition {
-  longitude: Number;
-  latitude: Number;
+    longitude: Number;
+    latitude: Number;
 }
 
 export interface IParkingSpot {
-  position: IPosition;
-  name: String;
-  distance: Number;
-  city: String;
-  owner: String;
+    position: IPosition;
+    name: String;
+    distance: Number;
+    city: String;
+    owner: String;
 }
