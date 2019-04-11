72cb1979b09a15418d064ea9ee32faa569804666
Vidar Magnusson <vidar.halmstad@hotmail.com>
2019-04-11 12:52:09 +0200

    Began work on list.

--- app/parkit/App.tsx
+++ app/parkit/App.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import data from "./assets/temp.json";
 import { IParkingSpot } from "./types/ParkingSpots.js";
+import List from "./use-cases/main/list/List.js";
 
 export default class App extends React.Component {
+    private spots: Array<IParkingSpot>;
+
     constructor(props: any) {
         super(props);
 
-        let spots = data.parkingspots as Array<IParkingSpot>;
+        this.spots = data.parkingspots as Array<IParkingSpot>;
     }
 
     public render() {
         return (
             <View style={styles.container}>
                 <Text>Open up App.tsx to start working on your app!</Text>
+                <List props={this.spots} />
             </View>
         );
     }
---
+++ app/parkit/use-cases/main/list/List.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { IParkingSpot } from "../../../App";
+
+export default class List extends React.Component<IProps> {
+    public render() {
+        return (
+            <View>
+                <Text />
+                {"ASDASD"}
+            </View>
+        );
+    }
+}
