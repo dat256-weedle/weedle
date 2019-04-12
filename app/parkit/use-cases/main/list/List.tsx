05cc8ccc0e34a32353a840ed88b8af4012bc4731
Praxzy <pontus.lindblom@hotmail.com>
2019-04-12 11:41:17 +0200

    Now shows list of dummy parking spots

--- app/parkit/use-cases/main/list/List.tsx
+++ app/parkit/use-cases/main/list/List.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { IParkingSpot } from "../../../App";
+import { View } from "react-native";
+import { IParkingSpot } from "../../../types/ParkingSpots";
+import ParkingElement from '../../../src/common/ParkingElement';
+
+
+interface IProps {
+    array: Array<IParkingSpot>
+}
 
 export default class List extends React.Component<IProps> {
+    constructor(props: IProps) {
+        super(props);
+    }
+
     public render() {
+        const array = this.props.array; 
         return (
-            <View>
-                <Text />
-                {"ASDASD"}
+            <View style={{display: "flex", flexDirection: "column"}}>
+            {
+                array.map((parkingspot, index) => (
+                    <ParkingElement key={index}
+                    distance={2}
+                    address="guld 2"
+                    price={20}
+                    rules="max 2h"
+                    provider="gbg park"
+                    image="any src"
+                    id="PA5252"
+                />
+                ))
+            }
             </View>
         );
     }
