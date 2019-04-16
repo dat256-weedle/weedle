956fa75373e5032c0d1ef013a1007c38e2e7215b
ChalmersSwexbe <erijohns@student.chalmers.se>
2019-04-17 00:13:46 +0200

    Move to current location on startup

--- app/parkit/common/map/ParkingSpotMap.tsx
+++ app/parkit/common/map/ParkingSpotMap.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import { inject, observer } from "mobx-react";
+import React, { RefObject } from "react";
 import MapView, { Marker } from "react-native-maps";
 import { Store } from "../../Store";
-import { observer, inject } from "mobx-react";
 
 interface IProps {
     store?: Store;
 }
 
 interface IState {
-    width: string,
+    width: string;
+}
+
+/**
+ * Region interface which matches MapViews region objects
+ */
+interface IRegion {
+    latitude: number;
+    longitude: number;
+    latitudeDelta: number;
+    longitudeDelta: number;
 }
 
 @inject("store")
@@ -16,6 +26,8 @@ interface IState {
 export default class ParkingSpotMap extends React.Component<IProps, IState> {
 
     private store: Store;
+    private theMap = React.createRef<MapView>();
+    private defaultLatLong = 0.092;
 
     constructor(props: IProps) {
         super(props);
@@ -23,29 +35,23 @@ export default class ParkingSpotMap extends React.Component<IProps, IState> {
         this.state = {
             width: "99%",
         };
+
     }
 
     public render() {
-        console.log(this.state)
         return (
             <MapView
-                style={{ 
+                style={{
                     alignItems: "center",
                     flex: 1,
                     width: this.state.width,
                 }}
-                // Show user location button isn't implemented with MapKit => use google instead
+                // Show user location button isn't implemented with Apple MapKit => use google instead
                 provider={"google"}
-                initialRegion={{
-                    latitude: 37.78825,
-                    latitudeDelta: 0.0922,
-                    longitude: -122.4324,
-                    longitudeDelta: 0.0421,
-                }}
-                mapPadding = {{top: 1, right: 1, bottom: 1, left: 1}}
-                showsUserLocation = {true}
-                showsMyLocationButton = {true}
-
+                ref={this.theMap}
+                mapPadding={{ top: 1, right: 1, bottom: 1, left: 1 }}
+                showsUserLocation={true}
+                showsMyLocationButton={true}
                 /**
                  * Stupid hack to make the 'show user location' button appear on android
                  *  from https://github.com/react-native-community/react-native-maps/issues/1033
@@ -60,6 +66,17 @@ export default class ParkingSpotMap extends React.Component<IProps, IState> {
 
             </MapView>
         );
+    }
+
+    public componentDidMount() {
+        navigator.geolocation.getCurrentPosition((position) => {
+            this.theMap.current!.animateToRegion({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    latitudeDelta: this.defaultLatLong,
+                    longitudeDelta: this.defaultLatLong,
+            }, 1);
+        });
     }
 
 }
