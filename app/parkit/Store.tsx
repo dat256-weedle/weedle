27947eef46b5c2e1494443c64b7056468b1d134a
ChalmersSwexbe <erik@erikjohnsson.se>
2019-04-19 12:36:38 +0200

    Prettier

--- app/parkit/Store.tsx
+++ app/parkit/Store.tsx
@@ -1,54 +1,51 @@
-import { action, observable } from "mobx";
-import data from "./assets/temp.json";
-import { IParkingSpot } from "./types/ParkingSpots.js";
+import { action, observable } from 'mobx';
+import data from './assets/temp.json';
+import { IParkingSpot } from './types/ParkingSpots.js';
 
 /**
  * Store which contains the state of the whole application
  */
 export class Store {
-
-    /**
+	/**
      * All parking spots which have been loaded by the application
      */
-    public allParkingSpots = data.parkingspots as IParkingSpot[];
+	public allParkingSpots = data.parkingspots as IParkingSpot[];
 
-    /**
+	/**
      * List of all parking spots which are being rented by the user
      */
-    @observable
-    public bookedParkingSpots: number[] = new Array();
+	@observable public bookedParkingSpots: number[] = new Array();
 
-    /**
+	/**
      * Number of actions executed on the store
      */
-    @observable
-    public numActions: number = 0;
+	@observable public numActions: number = 0;
 
-    /**
+	/**
      * Adds a parking spot to the bookedParkingSpots list
      * @param parkingSpot The parking spot to rent
      * @throws If parking spot with the same id is already booked by user
      */
-    @action
-    public bookParkingSpot(parkingSpot: number) {
-        if (this.bookedParkingSpots.includes(parkingSpot)) {
-            throw new Error("Parking spot already booked");
-        }
-        this.bookedParkingSpots.push(parkingSpot);
-        this.numActions++;
-    }
+	@action
+	public bookParkingSpot(parkingSpot: number) {
+		if (this.bookedParkingSpots.includes(parkingSpot)) {
+			throw new Error('Parking spot already booked');
+		}
+		this.bookedParkingSpots.push(parkingSpot);
+		this.numActions++;
+	}
 
-    /**
+	/**
      * Removes a parking spot from the bookedParkingsPots list.
      * @param parkingSpot The parking spot to finish renting
      * @throws If the parking spot is not already booked
      */
-    @action
-    public unBookParkingSpot(parkingSpot: number) {
-        if (!this.bookedParkingSpots.includes(parkingSpot)) {
-            throw new Error("Parking spot is not booked");
-        }
-        this.bookedParkingSpots = this.bookedParkingSpots.filter((item) => parkingSpot !== item);
-        this.numActions++;
-    }
+	@action
+	public unBookParkingSpot(parkingSpot: number) {
+		if (!this.bookedParkingSpots.includes(parkingSpot)) {
+			throw new Error('Parking spot is not booked');
+		}
+		this.bookedParkingSpots = this.bookedParkingSpots.filter((item) => parkingSpot !== item);
+		this.numActions++;
+	}
 }
--- app/parkit/common/map/ParkingSpotMap.tsx
+++ app/parkit/common/map/ParkingSpotMap.tsx
@@ -1,76 +1,71 @@
-import { inject, observer } from "mobx-react";
-import React from "react";
-import MapView, { Marker } from "react-native-maps";
-import { Store } from "../../Store";
+import { inject, observer } from 'mobx-react';
+import React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { Store } from '../../Store';
 
 interface IProps {
-    store?: Store;
+	store?: Store;
 }
 
 interface IState {
-    width: string;
+	width: string;
 }
 
-@inject("store")
+@inject('store')
 @observer
 export default class ParkingSpotMap extends React.Component<IProps, IState> {
+	private store: Store;
+	private theMap = React.createRef<MapView>();
+	private defaultLatLong = 0.092;
 
-    private store: Store;
-    private theMap = React.createRef<MapView>();
-    private defaultLatLong = 0.092;
+	constructor(props: IProps) {
+		super(props);
+		this.store = this.props.store!; // Since store is injected it should never be undefined
+		this.state = {
+			width: '99%'
+		};
+	}
 
-    constructor(props: IProps) {
-        super(props);
-        this.store = this.props.store!; // Since store is injected it should never be undefined
-        this.state = {
-            width: "99%",
-        };
-
-    }
-
-    public render() {
-        return (
-            <MapView
-                style={{
-                    alignItems: "center",
-                    flex: 1,
-                    width: this.state.width,
-                }}
-                // Show user location button isn't implemented with Apple MapKit => use google instead
-                provider={"google"}
-                ref={this.theMap}
-                mapPadding={{ top: 1, right: 1, bottom: 1, left: 1 }}
-                showsUserLocation={true}
-                showsMyLocationButton={true}
-                showsBuildings={false}
-                showsIndoorLevelPicker={false}
-                showsPointsOfInterest={false}
-                showsTraffic={false}
-                /**
+	public render() {
+		return (
+			<MapView
+				style={{
+					alignItems: 'center',
+					flex: 1,
+					width: this.state.width
+				}}
+				// Show user location button isn't implemented with Apple MapKit => use google instead
+				provider={'google'}
+				ref={this.theMap}
+				mapPadding={{ top: 1, right: 1, bottom: 1, left: 1 }}
+				showsUserLocation={true}
+				showsMyLocationButton={true}
+				showsBuildings={false}
+				showsIndoorLevelPicker={false}
+				showsPointsOfInterest={false}
+				showsTraffic={false}
+				/**
                  * Stupid hack to make the 'show user location' button appear on android
                  * from https://github.com/react-native-community/react-native-maps/issues/1033
                  */
-                onMapReady={() => this.setState({ width: "100%" })}>
-                {this.store.allParkingSpots.map((pSpot) => (
-                    <Marker
-                        coordinate={pSpot.position}
-                        key={pSpot.id}
-                    />
-                ))}
+				onMapReady={() => this.setState({ width: '100%' })}
+			>
+				{this.store.allParkingSpots.map((pSpot) => <Marker coordinate={pSpot.position} key={pSpot.id} />)}
+			</MapView>
+		);
+	}
 
-            </MapView>
-        );
-    }
-
-    public componentDidMount() {
-        navigator.geolocation.getCurrentPosition((position) => {
-            this.theMap.current!.animateToRegion({
-                    latitude: position.coords.latitude,
-                    latitudeDelta: this.defaultLatLong,
-                    longitude: position.coords.longitude,
-                    longitudeDelta: this.defaultLatLong,
-            }, 1);
-        });
-    }
-
+	public componentDidMount() {
+		navigator.geolocation.getCurrentPosition((position) => {
+			this.theMap.current!.animateToRegion(
+				{
+					latitude: position.coords.latitude,
+					latitudeDelta: this.defaultLatLong,
+					longitude: position.coords.longitude,
+					longitudeDelta: this.defaultLatLong
+				},
+				1
+			);
+		});
+	}
 }
