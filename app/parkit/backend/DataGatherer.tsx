589214c7cae2ca996ae4d4bcf5d0a3a3e17d98cc
Vidar Magnusson <vidar.halmstad@hotmail.com>
2019-04-20 18:11:32 +0200

    now merges the gathered data into a single array.

--- app/parkit/backend/DataGatherer.tsx
+++ app/parkit/backend/DataGatherer.tsx
@@ -5,83 +5,82 @@ const defaultPosition: IPosition = {
     longitude: 11.9845
 };
 
-export function getData(currentPosition: IPosition = defaultPosition) {
-    let completeArray = Array<IParkingSpot>();
-    completeArray.concat(getParkingGothenburgData());
-    completeArray.concat(getQParkData(currentPosition));
-    console.log("Complete data:::");
-    console.log(completeArray);
-}
-
-function promiseToObject(
-    promise: Promise<IParkingSpot[] | void>
-): Array<IParkingSpot> {
-    let result = Array<IParkingSpot>();
-    promise.then(data => {
-        try {
-            if (data instanceof Array) {
-                result = data as IParkingSpot[];
-            } else {
-                throw new Error("Invalid type");
-            }
-        } catch (error) {
-            console.log("Unable to parse promise data to IParkingSpot array");
-        }
+export async function getData(currentPosition: IPosition = defaultPosition) {
+    let completeArray: IParkingSpot[] = Array<IParkingSpot>();
+    await Promise.all([
+        getParkingGothenburgData(),
+        getQParkData(currentPosition)
+    ]).then((result: (IParkingSpot[] | void)[]) => {
+        result.forEach((data: IParkingSpot[] | void) => {
+            completeArray = tryConcat(data, completeArray);
+        });
     });
-    console.log("result:");
-    console.log(result);
-    return result;
 }
 
-function getParkingGothenburgData(): Array<IParkingSpot> {
+function tryConcat(
+    data: IParkingSpot[] | void,
+    completeArray: IParkingSpot[]
+): IParkingSpot[] {
+    try {
+        if (typeof data === "object") {
+            return completeArray.concat(data as IParkingSpot[]);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    return completeArray;
+}
+
+function getParkingGothenburgData(): Promise<IParkingSpot[] | void> {
     console.log("Gathering data from parking gothenburg...");
-    return promiseToObject(
-        fetch(
-            "https://www.parkeringgoteborg.se/api/parkings/besoksomraden?parkingtype=1&vehicletype=1",
-            {
-                method: "GET"
-            }
-        )
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                let arr = new Array<IParkingSpot>();
+    return fetch(
+        "https://www.parkeringgoteborg.se/api/parkings/besoksomraden?parkingtype=1&vehicletype=1",
+        {
+            method: "GET"
+        }
+    )
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            let arr = new Array<IParkingSpot>();
 
-                data.forEach((obj: any) => {
-                    let pos: IPosition = {
-                        longitude: obj.lng as number,
-                        latitude: obj.lat as number
-                    };
+            data.forEach((obj: any) => {
+                let pos: IPosition = {
+                    longitude: obj.lng as number,
+                    latitude: obj.lat as number
+                };
 
-                    // The distance should possibly be calculated in the future.
-                    let newObj: IParkingSpot = {
-                        name: obj.title,
-                        position: pos,
-                        id: obj.id,
-                        description: obj.id,
-                        distance: -1,
-                        provider: "ParkeringGothenburg",
-                        price: obj.regularPrice,
-                        specialPrice: obj.otherPrice,
-                        parkingSpots: obj.amountOfSpots
-                    };
+                // The distance should possibly be calculated in the future.
+                let newObj: IParkingSpot = {
+                    name: obj.title,
+                    position: pos,
+                    id: obj.id,
+                    description: obj.id,
+                    distance: -1,
+                    provider: "ParkeringGothenburg",
+                    price: obj.regularPrice,
+                    specialPrice: obj.otherPrice,
+                    parkingSpots: obj.amountOfSpots
+                };
 
-                    arr.push(newObj);
-                });
+                arr.push(newObj);
+            });
 
-                return arr;
-            })
-            .catch(error => {
-                console.error(error);
-            })
-    );
+            return arr;
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
 
-function getQParkData(currentPosition: IPosition): Array<IParkingSpot> {
+function getQParkData(
+    currentPosition: IPosition
+): Promise<IParkingSpot[] | void> {
     console.log("Gathering data from q-park...");
-    return promiseToObject(
-        fetch("https://www.q-park.se/api/websitesearch/GetParkingFacilities", {
+    return fetch(
+        "https://www.q-park.se/api/websitesearch/GetParkingFacilities",
+        {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -91,36 +90,36 @@ function getQParkData(currentPosition: IPosition): Array<IParkingSpot> {
                 latitude: currentPosition.latitude,
                 longitude: currentPosition.longitude
             })
+        }
+    )
+        .then(response => {
+            return response.json();
         })
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                let formattedArray = Array<IParkingSpot>();
-                data.ParkingFacilities.forEach((obj: any) => {
-                    let pos: IPosition = {
-                        longitude: obj.Longitude,
-                        latitude: obj.Latitude
-                    };
+        .then(data => {
+            let formattedArray = Array<IParkingSpot>();
+            data.ParkingFacilities.forEach((obj: any) => {
+                let pos: IPosition = {
+                    longitude: obj.Longitude,
+                    latitude: obj.Latitude
+                };
 
-                    let parkingSpot: IParkingSpot = {
-                        name: obj.Title,
-                        position: pos,
-                        id: obj.ID,
-                        description: obj.Description,
-                        distance: obj.Distance,
-                        provider: "Q-Park",
-                        price: "unknown",
-                        specialPrice: "unknown",
-                        parkingSpots: "unknown"
-                    };
+                let parkingSpot: IParkingSpot = {
+                    name: obj.Title,
+                    position: pos,
+                    id: obj.ID,
+                    description: obj.Description,
+                    distance: obj.Distance,
+                    provider: "Q-Park",
+                    price: "unknown",
+                    specialPrice: "unknown",
+                    parkingSpots: "unknown"
+                };
 
-                    formattedArray.push(parkingSpot);
-                });
-                return formattedArray;
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    );
+                formattedArray.push(parkingSpot);
+            });
+            return formattedArray;
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
