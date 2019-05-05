8c554f69a05c2e89df3b4b299cadde8c239b0b76
Praxzy <pontus.lindblom@hotmail.com>
2019-05-05 17:32:18 +0200

    Added prototype in Menu that works with .js but not .tsx

--- app/parkit/src/common/Menu.tsx
+++ app/parkit/src/common/Menu.tsx
@@ -1,8 +1,43 @@
-import React from "react";
-import { View } from "react-native";
-import { Button } from "react-native-elements";
-import Icon from "react-native-vector-icons/FontAwesome";
+import React from 'react';
+// import { BottomNavigation, Text } from "react-native-paper";
+import { Button } from "react-native-elements" // For old code
 
+/* Code that works with JavaScript but not TypeScript
+const Route1 = () => <Text>Placeholder1</Text>;
+const Route2 = () => <Text>Placeholder2</Text>;
+const Profile = () => <Text>Profile</Text>;
+
+export default class App extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'heart', title: 'Placeholder1', icon: 'favorite' },
+      { key: 'star', title: 'Placeholder2', icon: 'grade' },
+      { key: 'profile', title: 'Profile', icon: 'person' },
+    ],
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderScene = BottomNavigation.SceneMap({
+    heart: Route1,
+    star: Route2,
+    profile: Profile,
+  });
+
+  render() {
+    return (
+      <BottomNavigation
+        navigationState={this.state}
+        onIndexChange={this._handleIndexChange}
+        renderScene={this._renderScene}
+      />
+    );
+  }
+}
+*/
+
+// Old code that renders a button (WORKS)
 export default class Menu extends React.Component<any> {
 
     public render() {
@@ -11,68 +46,3 @@ export default class Menu extends React.Component<any> {
             )
         }
     }
-
-/*
-<Button title="Solid Button"/>
-
-<Button title="Clear button" type="clear"/>
-
-<Button title="Outline button" type="outline"/>
-*/
-
-/*
-<Button icon=
-  {<Icon name="arrow-right" size={15} color="white"/>}
-title="Button with icon component"/>
-
-<Button
-  icon={{
-    name: "arrow-right",
-    size: 15,
-    color: "white"
-  }}
-  title="Button with icon object"
-/>
-
-<Button
-  icon={
-    <Icon
-      name="arrow-right"
-      size={15}
-      color="white"
-    />
-  }
-  iconRight
-  title="Button with right icon"
-/>
-
-<Button title="Loading button" loading/>
-*/
-
-/*
-constructor () {
-    super()
-    this.state = {
-      selectedIndex: 2
-    }
-    this.updateIndex = this.updateIndex.bind(this)
-  }
-  
-  updateIndex (selectedIndex) {
-    this.setState({selectedIndex})
-  }
-  
-  render () {
-    const buttons = ['Hello', 'World', 'Buttons']
-    const { selectedIndex } = this.state
-  
-    return (
-      <ButtonGroup
-        onPress={this.updateIndex}
-        selectedIndex={selectedIndex}
-        buttons={buttons}
-        containerStyle={{height: 100}}
-      />
-    )
-  }
-  */
