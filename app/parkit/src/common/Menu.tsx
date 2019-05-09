e00827bece612b4d488fb0381b72c0f23714eb1b
Praxzy <pontus.lindblom@hotmail.com>
2019-05-09 13:27:06 +0200

    Made Menu.tsx follow code conventions

--- app/parkit/src/common/Menu.tsx
+++ app/parkit/src/common/Menu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { BottomNavigation, Text } from "react-native-paper";
 
 const Route1 = () => <Text>Placeholder1</Text>;
@@ -12,34 +12,34 @@ interface IState {
 
 export default class Menu extends React.Component<any,IState> {
 
+  private renderScene = BottomNavigation.SceneMap({
+    heart: Route1,
+    star: Route2,
+    profile: Profile,
+  });
+
   constructor(props: any){
     super(props)
     this.state = {
       index: 0,
       routes: [
-        { key: 'heart', title: 'Placeholder1', icon: 'favorite' },
-        { key: 'star', title: 'Placeholder2', icon: 'grade' },
-        { key: 'profile', title: 'Profile', icon: 'person' },
+        { key: "heart", title: "Placeholder1", icon: "favorite" },
+        { key: "star", title: "Placeholder2", icon: "grade" },
+        { key: "profile", title: "Profile", icon: "person" },
       ],
     };
   }
-
+  
   public render() {
     return (
       <BottomNavigation
         style={{width: "100%"}}
         navigationState={this.state}
-        onIndexChange={this._handleIndexChange}
-        renderScene={this._renderScene}
+        onIndexChange={this.handleIndexChange}
+        renderScene={this.renderScene}
       />
     );
   }
 
-  private _handleIndexChange = (index: number) => this.setState({ index });
-
-  private _renderScene = BottomNavigation.SceneMap({
-    heart: Route1,
-    star: Route2,
-    profile: Profile,
-  }); 
+  private handleIndexChange = (index: number) => this.setState({ index });
 }
