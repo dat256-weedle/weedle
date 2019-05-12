a4601b8bbaf85fd297a1a935b24e7fb8418a278f
ChalmersSwexbe <erijohns@student.chalmers.se>
2019-05-12 19:25:03 +0200

    Fixed RentButton.test imports

--- app/parkit/__test__/common/RentButton.test.tsx
+++ app/parkit/__test__/common/RentButton.test.tsx
@@ -1,14 +1,14 @@
 import { mount } from "enzyme";
 import React from "react";
 import { Button } from "react-native-material-ui";
-import { IParkingSpot, Providers } from "types";
 import { Store } from "../../src/backend/store/Store";
 import RentButton from "../../src/common/rentpage/RentButton";
+import { IParkingSpot, Providers } from "../../src/types";
 
 const id = (Math.random() * Number.MAX_VALUE).toString();
 const store = new Store();
-const parkingSpot: IParkingSpot = {id: id, name: "", position: {latitude: 0, longitude: 0}, description: "", price: "", specialPrice: "", parkingSpots: "", distance: 0, provider: Providers.EasyPark}
-let app = mount(<RentButton  store={store} parkingSpot={parkingSpot} car="car" isParked={false} />);
+const parkingSpot: IParkingSpot = {id, name: "", position: {latitude: 0, longitude: 0}, description: "", price: "", specialPrice: "", parkingSpots: "", distance: 0, provider: Providers.EasyPark}
+const app = mount(<RentButton  store={store} parkingSpot={parkingSpot} car="car" isParked={false} />);
 
 it("Should add sessions to the store when clicked", () => {
     app.find(Button).props().onPress!();
@@ -19,7 +19,7 @@ it("Should add sessions to the store when clicked", () => {
 it("Should remove sessions from the store when clicked again", () => {
     expect(store.currentParkingSessions.find((item) => item.parkingSpot.id === id)).toBeDefined();
     
-    let app = mount(<RentButton  store={store} parkingSpot={parkingSpot} car="car" isParked={true} />);
+    const app = mount(<RentButton  store={store} parkingSpot={parkingSpot} car="car" isParked={true} />);
 
     app.find(Button).props().onPress!();
 
