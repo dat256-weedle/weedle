77f4bb784ad22646d3a0c193efac5b01504b5009
Praxzy <pontus.lindblom@hotmail.com>
2019-05-24 20:14:10 +0200

    An attempt to make placeholder have a different style (read description)
    The code includes a buggy solution to let placeholder have a different textStyle. The bug: every time the style changes, the already written text is cleared.
    
    Furthest down in the file is the old working code when the placeholder and written text have the same textStyle.

--- app/parkit/src/usage/userpage/User.tsx
+++ app/parkit/src/usage/userpage/User.tsx
@@ -1,25 +1,69 @@
-import { inject, observer } from "mobx-react";
 import React, { Component } from "react";
 import {
     Image,
     StyleSheet,
-    Text,
     TextInput,
-    TouchableOpacity,
     View
 } from "react-native";
-import { Store } from "../../backend/store/Store";
 
+interface IState {
+    hasText: boolean;
+}
 
-export default class User extends Component<any> {
+export default class User extends Component<IState> {
+    constructor(props: Readonly<IState>) {
+        super(props);
+        this.state = {hasText: false}
+    }
+
     public render() {
+        return (
+        <View style={{ alignItems: "center", backgroundColor: "rgb(100,210,110)" }}>
+            <Image source={require("../../../assets/user.png")} style={{
+                height: 70, width: 70, marginTop: 10, marginBottom: 10}}/>
+            <View style= {{alignItems: "center", marginTop: 10, marginBottom: 10}}>
+                <TextInput
+                    placeholder = "Enter name"
+                    // If there is any input, use normal style. Otherwise use placeholder-style.
+                    style={this.state.hasText ? styles.textstyle : placeholderStyles.textstyle}
+                    // Checks if there is any text everytime the text is changed
+                    onChangeText={(text) => this.setState({hasText: text.length !== 0})} />
+            </View>
+        </View>
+    )}
+}
+
+const styles = StyleSheet.create({
+    textstyle: {
+        color: "black",
+        fontSize: 25,
+        textAlign: "center",
+        fontWeight: "bold"
+    }
+})
+
+const placeholderStyles = StyleSheet.create({
+    textstyle: {
+        color: "black",
+        fontSize: 25,
+        textAlign: "center"
+    }
+})
+
+
+// Old working code that doesn't have a different style for the placeholder text
+/*    public render() {
         return(
         <View style={{ alignItems: "center", backgroundColor: "rgb(100,210,110)" }}>
                     <Image source={require("../../../assets/user.png")} style={{
                         height: 70, width: 70, marginTop: 10, marginBottom: 10}}/>
                     <View style= {{alignItems: "center", marginTop: 10, marginBottom: 10}}>
-                        <Text style={{color: "black", fontSize: 30, fontWeight: "bold"}}>John Doe </Text>
+                        <TextInput
+                            style={{color: "black", fontSize: 25, fontWeight: "bold", textAlign: "center"}}
+                            placeholder="Enter username "
+                            // placeholderTextColor="rgb(200,100,150)"
+                            />
                     </View>
                 </View>
-        )}      
-}
+    )}
+*/      
