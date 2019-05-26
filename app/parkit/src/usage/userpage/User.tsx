c649b6ebec86c01075e4622891e0ab682dd2e74c
Praxzy <pontus.lindblom@hotmail.com>
2019-05-26 19:06:58 +0200

    Removed placeholderStyle

--- app/parkit/src/usage/userpage/User.tsx
+++ app/parkit/src/usage/userpage/User.tsx
@@ -1,12 +1,8 @@
 import { Store } from "backend/store/Store";
 import { inject, observer } from "mobx-react";
 import React, { Component } from "react";
-import { Image, StyleSheet, TextInput, View } from "react-native";
+import { Image, TextInput, View } from "react-native";
 import { secondarycolor } from "../../styles";
-
-interface IState {
-    hasText: boolean;
-}
 
 interface IProps {
     store?: Store;
@@ -14,7 +10,7 @@ interface IProps {
 
 @inject("store")
 @observer
-export default class User extends Component<IProps, IState> {
+export default class User extends Component<IProps> {
     private store: Store;
 
     constructor(props: IProps) {
@@ -63,15 +59,13 @@ export default class User extends Component<IProps, IState> {
                     <TextInput
                         value={this.store.userName}
                         placeholder="Enter name"
-                        // If there is any input, use normal style. Otherwise use placeholder-style.
-                        style={
-                            this.state.hasText
-                                ? styles.textstyle
-                                : placeholderStyles.textstyle
-                        }
-                        // Checks if there is any text everytime the text is changed
+                        style={{
+                            color: "white",
+                            fontSize: 25,
+                            fontWeight: "bold",
+                            textAlign: "center"
+                        }}
                         onChangeText={text => {
-                            this.setState({ hasText: text.length !== 0 });
                             this.store.setUserName(text);
                         }}
                     />
@@ -80,36 +74,3 @@ export default class User extends Component<IProps, IState> {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    textstyle: {
-        color: "white",
-        fontSize: 25,
-        textAlign: "center",
-        fontWeight: "bold"
-    }
-});
-
-const placeholderStyles = StyleSheet.create({
-    textstyle: {
-        color: "white",
-        fontSize: 25,
-        textAlign: "center"
-    }
-});
-
-
-// Old working code that doesn't have a different style for the placeholder text
-/*    public render() {
-        return(
-        <View style={{ alignItems: "center", backgroundColor: "rgb(100,210,110)" }}>
-                    <Image source={require("../../../assets/user.png")} style={{
-                        height: 70, width: 70, marginTop: 10, marginBottom: 10}}/>
-                    <View style= {{alignItems: "center", marginTop: 10, marginBottom: 10}}>
-                        <TextInput
-                            style={{color: "white", fontSize: 25, fontWeight: "bold", textAlign: "center"}}
-                            placeholder="Enter username" />
-                    </View>
-                </View>
-    )}
-*/
